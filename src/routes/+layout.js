1c6eb6b3d775b59e566d078d6cce2083e43c4a4f
Cooper <Cooperransom08@gmail.com>
2025-04-12 02:03:41 +0000

    fix stuff :thumbs_up:

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,7 @@
 // Makes cloudflare work ig
 export const prerender = true;
 export const ssr = true;
+export const csr = false;
 export const trailingSlash = "never";
 export const config = {
   runtime: "edge",
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1018,7 +1018,7 @@
       </script>
     </div>
     <footer class="text-center text-type-dimmed text-sm">
-      <p class="flex justify-center items-center space-x-2">
+      <p class="flex flex-row justify-center items-center gap-2">
         <!-- svelte-ignore a11y_consider_explicit_label -->
         <a
           href="https://github.com/itzcozi/stremio-guide"
@@ -1048,6 +1048,17 @@
               d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z"
             ></path>
           </svg>
+        </a>
+        <!-- svelte-ignore a11y_consider_explicit_label -->
+        <a
+          href="https://t.me/+B3UIJrNsWf0wNzJh"
+          class="hover:scale-105 text-dark transition duration-200"
+          title="Telegram Group">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            class="w-5 h-5 fill-current"
+            ><path d="M23.001 1.999L0 13 6.031 15.307 17 8 10.687 17.552 20 22z"></path></svg>
         </a>
         <!-- svelte-ignore a11y_consider_explicit_label -->
         <a
