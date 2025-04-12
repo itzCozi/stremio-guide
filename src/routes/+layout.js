78324ef3f9c0c916d69f943721b6ad26c6d9a247
Cooper <Cooperransom08@gmail.com>
2025-04-12 02:24:41 +0000

    Bruh there is kinda a big bug and idk where it comes from :skull:

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,7 +1,6 @@
 // Makes cloudflare work ig
 export const prerender = true;
 export const ssr = true;
-export const csr = false;
 export const trailingSlash = "never";
 export const config = {
   runtime: "edge",
